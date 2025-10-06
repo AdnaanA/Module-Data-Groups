495569c0f5f5da306141fa7c23a4f8b53fe82a9a
AdnaanA <thetriplex247@gmail.com>
2025-10-06 10:13:18 +0100

    includes function has been refactored and still using for...of loop

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,12 @@
 // Refactor the implementation of includes to use a for...of loop
 
-function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
-      return true;
+function includes(list, target) {  // list is an array
+  for (const element of list) {  // Using for...of loop to iterate through each element in the array
+    if (element === target) {  // Checking if the current element matches the target value
+      return true;  // Return true if a match is found
     }
   }
-  return false;
+  return false;  // Return false if no match is found after checking all elements
 }
 
 module.exports = includes;
